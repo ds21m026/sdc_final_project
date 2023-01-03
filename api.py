524ff9245e908c6f6e0c1709a7f0d259050f6bcf
ds21m026 <114813357+ds21m026@users.noreply.github.com>
2023-01-03 17:57:05 +0100

    updated
    added comments

--- api.py
+++ api.py
@@ -1,45 +1,33 @@
-from fastapi import FastAPI # import FastAPI class
+# import python modules
+from fastapi import FastAPI 
 from pydantic import BaseModel
-
 import numpy as np
 import pandas as pd
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 
+# create instance of fastapi
 api = FastAPI()
 
 # using Pydantic models to declare request body
 # base data as derived from Streamlit Frontend
 class titanicData(BaseModel):
-	Age: int
+	age: int
 	pclassAux: int
 	family_size: int
 	sex: str
 	embarked: str
-	
-# preprocessed data as returned from API
-class modelData(BaseModel):
-	Age: int
-	child: int
-	family_size: int
-	Pclass_1: int
-	Pclass_2: int
-	Pclass_3: int
-	Sex_female: int
-	Sex_male: int
-	Embarked_C: int
-	Embarked_Q: int
-	Embarked_S: int
 
-
-# define post method to preprocess data
-# define post method to get prediction from model
+# define post method to use the input values 
+# get prediction from model
 @api.post("/predict/")
 def predict_proba(data: titanicData):
 	
+	# initialize default array
 	arr = [0, 0, 0, 0,0,0, 0,0, 0,0,0]
-	# child indicator
-	arr[1] = int(data.Age <= 16)
+	
+	# child
+	arr[1] = int(data.age <= 16)
 	
 	# passenger class
 	if data.pclassAux==1:
@@ -49,15 +37,13 @@ def predict_proba(data: titanicData):
 	if data.pclassAux==3:
 	    arr[5]=1
 	    
-	# male/female indicators
-	Sex_female = 0
-	Sex_male = 0
+	# male/female 
 	if data.sex=="female":
 	    arr[6]=1
 	else:
 	    arr[7]=1
 	
-	# embarked indicators
+	# embarked
 	if data.embarked=="Cherbourg":
 	    arr[8] = 1
 	if data.embarked=="Queenstown":
@@ -65,17 +51,16 @@ def predict_proba(data: titanicData):
 	if data.embarked=="Southampton":
 	    arr[10] = 1
 	
-
 	
 	# load the Random Forest Classifier
+	# pickle file from https://github.com/tlary/Kaggle_Titanic
 	with open("./rfSimple.pkl", 'rb') as file:
     	    rf = pickle.load(file)
-    # make predictions
-	#SurvivalProba = rf.predict_proba(inputDF)[0,1]
-	#test = [[23,0,3,1,0,0,1,0,1,0,0]]
+    
+	# make predictions
 	arr = [arr]
 	SurvivalProba = rf.predict_proba(arr)[0,1]
-	#survPerc = round(SurvivalProba*100, 1)
+	SurvivalProba = round(SurvivalProba * 100, 2)
 	
+	# return probability to survive
 	return SurvivalProba
-
--- streamlit.py
+++ streamlit.py
@@ -1,51 +1,40 @@
+# import python modules
 import streamlit as st
 import pandas as pd
 import requests
 import json
-from pydantic import BaseModel
 
-# using Pydantic models to declare request body
-# base data as derived from Streamlit Frontend
-class titanicData(BaseModel):
-	Age: int
-	pclassAux: int
-	family_size: int
-	sex: str
-	embarked: str
-	
-# preprocessed data as returned from API
-class modelData(BaseModel):
-	Age: int
-	child: int
-	family_size: int
-	Pclass_1: int
-	Pclass_2: int
-	Pclass_3: int
-	Sex_female: int
-	Sex_male: int
-	Embarked_C: int
-	Embarked_Q: int
-	Embarked_S: int
+st.header("placeholder visualization")
 
+st.write("tbd")
 
-# get input data from streamlit frontend
-Age = st.sidebar.number_input("How old are you?", 0, 100, 30)
-family_size = st.sidebar.number_input("How many family members are aboard the ship (including yourself)?", 1, 20, 1)
-pclassAux = st.sidebar.selectbox("In which passenger class are you traveling?", (1,2,3))
-sex = st.sidebar.selectbox("Are you male or female?", ("male", "female"), index=1)
-embarked = st.sidebar.selectbox("Which is your port of Embarkation?", ("Cherbourg", "Queenstown", "Southampton"))
+st.header("prediction via fastapi")
 
-# combine input to dict
-data = {"Age": Age,
+# get input data from different parameters in streamlit frontend
+age = st.number_input("How old are you?", 0, 100, 30)
+family_size = st.number_input("How many family members are on board (including yourself)?", 1, 20, 1)
+pclassAux = st.selectbox("In which passenger class are you traveling?", (1,2,3))
+sex = st.selectbox("Are you male or female?", ("male", "female"), index=1)
+embarked = st.selectbox("Which is your port of embarkation?", ("Cherbourg", "Queenstown", "Southampton"))
+
+# combine input to dictionary
+data = {"age": age,
 	"family_size": family_size,
 	"pclassAux": pclassAux,
 	"sex": sex,
 	"embarked": embarked}
-dataJSON = json.dumps(data) # create json object from dict
-st.write(dataJSON)
-# preprocess data by making post request to the API
-r = requests.post(url = "http://localhost:8000/predict/", data=dataJSON)
 
-st.write("Your chance of Survival based on the information provided is: ", r.text)
+# create json object from dict	
+dataJSON = json.dumps(data)
 
-	
+# button to show json object
+if st.button("show inputs as json"):
+	st.write(dataJSON)
+
+# button to call the api
+if st.button("get prediction"):
+	# post request to the API
+	r = requests.post(url = "http://localhost:8000/predict/", data=dataJSON)
+
+	# get result from api
+	st.write("Your chance to survive based on the information in %: ", r.text)
